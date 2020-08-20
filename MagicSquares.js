a1b0145c73a6229811208ff10e7ba4f29ac33552
Maher <mahermarawi92@gmail.com>
2020-08-20 20:47:53 +0200

    commit

--- MagicSquares.js
+++ MagicSquares.js
@@ -1,31 +1,45 @@
+//pseudocode
+//declar a function that take an array
+//defind two variables to calculate the Diameters
+//for loop that start from 0 to the length of the array
+//if the diameters are equal return true otherwise return false
 
-
-
+//code:
 function MagicSquares(arr){
-        if ((arr[0] + arr[4] + arr[8]) == (arr[2] + arr[4] + arr[6])) {
-            return true;
-        }
-        else{
-            return false
-        }
+    var x = 0 ;
+    var y = 0 ;
+    for (let i = 0 ; i < arr.length ; i++) {
+        x += arr[i][i]
+        y += arr[i][(arr.length-i-1)]
+    }
+    if (x == y) {
+        return true;
+    }else{
+        return false;
+    }
 }
+let ar0=[
+    [5,9],
+    [3,7]
+]
+var ar1 = [
+    [3, 5, 7], 
+    [8, 1, 6], 
+    [4, 9, 2] 
+]
+var ar4 = [
+    [58,37,48,85],
+    [12,37,43,96],
+    [12,37,43,96],
+    [59,37,43,86]
+]
+console.log(MagicSquares(ar0))
+console.log(MagicSquares(ar1))
 
-var ar1 = [2, 7, 6, 
-           9, 5, 1, 
-           4, 3, 8]
+//diagram
+//i = 0 => x[0][0] = 5 , y[0][1] = 9
+//i = 0 => x[1][1] = 3 , y[1][0] = 7
+// return true
 
-var ar2 = [3, 5, 7, 
-           8, 1, 6, 
-           4, 9, 2]
-
-var ar3 = [2, 1, 6, 
-           3, 5, 7, 
-           4, 9, 2]
-
-var ar4 = [8, 1, 6,
-           7, 5, 3, 
-           4, 9, 2]
-
-console.log(MagicSquares(ar4))
 
 
