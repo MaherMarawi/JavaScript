b042d21c713fe642ad3df5f9faffc267d931b19d
Maher <mahermarawi92@gmail.com>
2020-08-21 14:24:38 +0200

    commit

---
+++ SelectionSort.js
@@ -0,0 +1,29 @@
+//pseudocode
+//declare a function that take an array
+
+//code:
+function BubbleSort(arr) {
+    var x = 0;
+    var min = arr[0];
+    var y = 0;
+    let j = 0
+        for (let i = 0; i < arr.length; i++) {
+            j = i ;
+            min = arr[i]
+            y = arr[i]
+            x = i
+            while( j <= arr.length) {
+                if (arr[j] < min) {
+                    min = arr[j]
+                    x = j 
+                }
+                j++
+            }
+            y = arr[i]
+            arr[i] = min
+            arr[x] = y
+        }
+    return arr
+}
+var numbs = [91 ,5 ,12 ,90 ,11 ,7 ]
+console.log(BubbleSort(numbs))
